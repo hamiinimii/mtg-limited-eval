7d13316a3d9c045d3baf3ed09dbaac4a79a24296
hamiinimii <34637494+hamiinimii@users.noreply.github.com>
2022-01-30 10:46:00 +0900

    before dragdrop test

---
+++ src/dragdrop.js
@@ -0,0 +1,21 @@
+// when starting drag
+function dragstart(event){
+  console.log("dragstart");
+  console.log(event)
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+// drop
+function drop(event){
+  var dragged_id = event.dataTransfer.getData("text");
+  var drag_elm = document.getElementById(dragged_id);
+  console.log(dragged_id);
+  console.log(drag_elm);
+  event.currentTarget.appendChild(drag_elm);
+  event.preventDefault(); // cancel drop event in order to avoid error
+}
+
+// if dragged element is over dropped element
+function dragover(event){
+  event.preventDefault();
+}
--- src/json2cards.js
+++ src/json2cards.js
@@ -1,6 +1,7 @@
 function jsonToCards(cardJson){
   // listElement = document.createElement('ul');
   // listElement.id = 'cardlist';
+  const divNotRankedElement = document.querySelector('#unranked > div');
 
   // for (let i = 0; i < cardJson.length; i++) {
   for (let i = 0; i < 30; i++) {
@@ -13,33 +14,36 @@ function jsonToCards(cardJson){
       continue;
     }
 
-    // const articleElement = document.createElement('li');
-    const articleElement = document.createElement('div');
-    const h2Element = document.createElement('h2');
-    const p1Element = document.createElement('p');
-    const p2Element = document.createElement('p');
+    // const divCardElement = document.createElement('li');
+    let divCardElement = document.createElement('div');
     const hrefImgElement = document.createElement('a');
     const img1Element = document.createElement('img');
 
-    articleElement.className = 'card_article';
-    // h2Element.textContent = card.name;
-    // p1Element.textContent = 'manaCost: ' + card.manaCost;
-    // p2Element.textContent = 'type: ' + card.type;
+    divCardElement.className = 'card_div';
+    divCardElement.id = card.identifiers.scryfallId;
+    divCardElement.draggable = 'true';
+    divCardElement.setAttribute('ondragstart', 'dragstart(event)');
+
+    // card visualization
     hrefImgElement.href = 'https://api.scryfall.com/cards/' + card.identifiers.scryfallId + '?format=image&face=front';
     hrefImgElement.dataset.lightbox = card.printings[0];
+    hrefImgElement.dataset.title = card.name;
     img1Element.src = 'https://api.scryfall.com/cards/' + card.identifiers.scryfallId + '?format=image&face=front';
-    img1Element.width = 150; // 横サイズ（px）
+    img1Element.width = 150;
+    // img1Element.setAttribute('ondragstart', 'dragover(event)');
+    // hidden card parameters
+    hrefImgElement.dataset.c_name = card.name;
+    hrefImgElement.dataset.c_manacost = card.manaCost;
+    hrefImgElement.dataset.c_type = card.type;
+    // pt does not refer back side now
+    if (card.power) hrefImgElement.dataset.c_power = card.power;
+    if (card.toughness) hrefImgElement.dataset.c_toughness = card.toughness;
 
-    // articleElement.appendChild(h2Element);
-    // articleElement.appendChild(p1Element);
-    // articleElement.appendChild(p2Element);
-    articleElement.appendChild(hrefImgElement);
+    divCardElement.appendChild(hrefImgElement);
     hrefImgElement.appendChild(img1Element);
 
-    section.appendChild(articleElement);
-    // listElement.appendChild(articleElement);
-    // section.appendChild(listElement);
-    console.log(i)
+    divNotRankedElement.appendChild(divCardElement);
+    // console.log(i)
 
   }
 
