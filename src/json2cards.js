3e5b4fb119cca6b87b453cc1265e0ad4089b8777
hamiinimii <34637494+hamiinimii@users.noreply.github.com>
2022-01-30 11:30:41 +0900

    drag and drop

--- src/json2cards.js
+++ src/json2cards.js
@@ -1,7 +1,7 @@
 function jsonToCards(cardJson){
   // listElement = document.createElement('ul');
   // listElement.id = 'cardlist';
-  const divNotRankedElement = document.querySelector('#unranked > div');
+  const divNotRankedElement = document.querySelector('#tier4 > div');
 
   // for (let i = 0; i < cardJson.length; i++) {
   for (let i = 0; i < 30; i++) {
@@ -15,21 +15,26 @@ function jsonToCards(cardJson){
     }
 
     // const divCardElement = document.createElement('li');
-    let divCardElement = document.createElement('div');
+    // let divCardElement = document.createElement('div');
     const hrefImgElement = document.createElement('a');
     const img1Element = document.createElement('img');
 
-    divCardElement.className = 'card_div';
-    divCardElement.id = card.identifiers.scryfallId;
-    divCardElement.draggable = 'true';
-    divCardElement.setAttribute('ondragstart', 'dragstart(event)');
+    // divCardElement.className = 'card_div';
+    // divCardElement.id = card.identifiers.scryfallId;
+    // divCardElement.draggable = 'true';
+    // divCardElement.setAttribute('ondragstart', 'dragstart(event)');
 
+    hrefImgElement.id = card.identifiers.scryfallId;
+    hrefImgElement.draggable = 'true';
+    hrefImgElement.setAttribute('ondragstart', 'dragstart(event)');
     // card visualization
     hrefImgElement.href = 'https://api.scryfall.com/cards/' + card.identifiers.scryfallId + '?format=image&face=front';
     hrefImgElement.dataset.lightbox = card.printings[0];
     hrefImgElement.dataset.title = card.name;
+
     img1Element.src = 'https://api.scryfall.com/cards/' + card.identifiers.scryfallId + '?format=image&face=front';
     img1Element.width = 150;
+    img1Element.id = hrefImgElement.id; // use same id to make parent draggable
     // img1Element.setAttribute('ondragstart', 'dragover(event)');
     // hidden card parameters
     hrefImgElement.dataset.c_name = card.name;
@@ -39,10 +44,10 @@ function jsonToCards(cardJson){
     if (card.power) hrefImgElement.dataset.c_power = card.power;
     if (card.toughness) hrefImgElement.dataset.c_toughness = card.toughness;
 
-    divCardElement.appendChild(hrefImgElement);
+    // divCardElement.appendChild(hrefImgElement);
     hrefImgElement.appendChild(img1Element);
 
-    divNotRankedElement.appendChild(divCardElement);
+    divNotRankedElement.appendChild(hrefImgElement);
     // console.log(i)
 
   }
