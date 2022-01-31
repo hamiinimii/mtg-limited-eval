ee833041aab1f7c45be8d6c7de1b2d2cbde83b0b
hamiinimii <34637494+hamiinimii@users.noreply.github.com>
2022-01-31 23:46:12 +0900

    implemented type filter

--- src/sidemenu.js
+++ src/sidemenu.js
@@ -6,99 +6,59 @@ $('.btn_menu').on('click', () => {
 
 // trigger color filtering
  $('input:checkbox').change(filterchange);
- // $('input:checkbox.colorcheck').change(colorchange);
- // $('input:checkbox.raritycheck').change(raritychange);
- // $('input:checkbox.typecheck').change(typechange);
-
 
  // color filtering function
  function filterchange(){
-   let visible = false;
+   let visible = [];
+   // data variables
    let color = '';
    let rarity = '';
    let types = '';
+   let types_front = [];
+   let types_back = [];
+
    $('.card_div').each(function(i, o){
      color = $(o).children('a').attr('data-c_color').replace(/,/g, '');
      rarity = $(o).children('a').attr('data-c_rarity');
-     visible = false;
+     visible = [false, false, true]; // initialize visibility
      // check color
      if ($('.colorcheck-multi').prop("checked")==true){
        for (let i = 0; i < color.length; i++){
          if ($('.'+color[i]).prop('checked')==true){
-           visible = true;
+           visible[0] = true;
            break;
          }
        }
      }else{ // not showing multicolored unless all colors are included
-       visible = true;
+       visible[0] = true;
        for (let i = 0; i < color.length; i++){
          if ($('.'+color[i]).prop('checked')==false){
-           visible = false;
+           visible[0] = false;
            break;
          }
        }
      }
-     // check rarity
-     if ($('.'+rarity).prop('checked')==false){
-       visible = false;
+     // check types
+     types_front = $(o).children('[data-c_face=front]').attr('data-c_types').split(',');
+     if ($(o).children('[data-c_face=back]').length && $('.typecheck-multi').prop('checked')==true) { // transform cards
+       types_back = $(o).children('[data-c_face=back]').attr('data-c_types').split(',');
+       Array.prototype.push.apply(types_front, types_back);
      }
-     if (visible){
+     console.log(types_front);
+     for (let i = 0; i < types_front.length; i++){
+       if ($('.'+types_front[i]).prop('checked')==true){
+         visible[1] = true; // by default visible[1] is false and only in this case it becomes true
+         break;
+       }
+     }
+     // check rarity
+     // if ($('.'+rarity).prop('checked')==false){
+     visible[2] = $('.'+rarity).prop('checked');
+     // }
+     // result
+     if (visible.every(value => value==true)){
        $(o).show();
      } else {
-       $(o).hide();
-     }
-   })
- }
-
- // color filtering function
- function colorchange(){
-   let visible = false;
-   let color = '';
-   $('.card_div').each(function(i, o){
-     color = $(o).children('a').attr('data-c_color').replace(/,/g, '');
-     visible = false;
-     for (let i = 0; i < color.length; i++){
-       if ($('.'+color[i]).prop('checked')==true){
-         $(o).show();
-         visible = true;
-         break;
-       }
-     }
-     if (!visible){
-       $(o).hide();
-     }
-   })
- }
-
- // rarity filtering function
- function raritychange(){
-   let rarity = '';
-   $('.card_div').each(function(i, o){
-     rarity = $(o).children('a').attr('data-c_rarity');
-     console.log(rarity);
-       if ($('.'+rarity).prop('checked')==true){
-         $(o).show();
-       } else {
-         $(o).hide();
-       }
-     })
- }
-
- // type filtering function
- function typechange(){
-   let visible = false;
-   let types = '';
-   $('.card_div').each(function(i, o){
-     types = $(o).children('[data-c_face=front]').attr('data-c_types').split(',');
-     console.log(types);
-     for (let i = 0; i < types.length; i++){
-       if ($('.'+types[i]).prop('checked')==true){
-         $(o).show();
-         visible = true;
-         break;
-       }
-     }
-     if (!visible){
        $(o).hide();
      }
    })
